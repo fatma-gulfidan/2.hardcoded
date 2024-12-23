04df31ee2a31ff0234791ab4691e1153535fa7a5
Fatma Gülfidan <92696695+fatma-gulfidan@users.noreply.github.com>
2024-12-23 13:51:15 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,3 @@
 # secrets.py
 ACCESS_TOKEN = "ghp_1234567890abcdefghijklmnopqrstuvwxyz1234"  # Fake GitHub token
-AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"  # Fake AWS secret key
+AWS_SECRET_KEY = "AKIAIOSFODNN7EXAMPLE"  # Fake AWS secret keyyyyy
